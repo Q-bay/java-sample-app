7b0131dd949351e395602db80921cf4e3f4caef5
Quality0610 <kzktcy0610@gmail.com>
2021-09-14 21:45:36 +0900

    Entity修正

--- sample1/sample_app1/src/main/java/com/example/demo/repository/entity/HogeEntity.java
+++ sample1/sample_app1/src/main/java/com/example/demo/repository/entity/HogeEntity.java
@@ -5,8 +5,10 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @DynamoDBTable(tableName = "hoge")
 @Data
 public class HogeEntity {
