3c34e3d11fd39d73245dc95cbdcad0731b430c55
Quality0610 <kzktcy0610@gmail.com>
2021-10-12 00:06:49 +0900

    データ取得成功

--- sample2/sample_app2/src/main/java/com/example/demo/SampleApp2Application.java
+++ sample2/sample_app2/src/main/java/com/example/demo/SampleApp2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example")
+@MapperScan(basePackages = "com.example.demo.domain.repository")
 public class SampleApp2Application {
 
 	public static void main(String[] args) {
